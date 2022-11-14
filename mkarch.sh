dc95cfad674c8587041fc4800c555cfdea868d43
Danel Sibaev <danon@DESKTOP-EHLFN9L.localdomain>
2022-11-15 00:32:07 +0300

    try

--- mkarch.sh
+++ mkarch.sh
@@ -8,23 +8,31 @@ do
     esac
 done
 
-#echo "Path: $dir_path";
-#echo "Name: $name";
-
 touch $name.sh
 
-tar -cvzf $name.tar.gz $dir_path 
+base64res=$(tar -cz $dir_path | base64)
 
+echo "#!/bin/bash" >> $name.sh
 echo "while getopts o: flag; do" >> $name.sh
 echo "	case \${flag} in" >> $name.sh
 echo "		o) output_dir=\${OUTARG};;" >> $name.sh
 echo "	esac" >> $name.sh
 echo "done" >> $name.sh
 
-echo -n "base64 -d " >> $name.sh
-base64 $name.tar.gz >> $name.sh
-echo -n 'tar -xvf ' >> $name.sh
-echo -n ${name} >> $name.sh
-echo '.tar.gz' >> $name.sh
+echo "res=\"$base64res\"" >> $name.sh
+
+echo "if [ \$output_dir ]" >> $name.sh
+echo "then" >> $name.sh
+echo "mkdir \$output_dir" >> $name.sh
+echo "else" >> $name.sh
+echo "mkdir ${name}" >> $name.sh
+echo "fi" >> $name.sh
+
+echo "if [ \$output_dir ]" >> $name.sh
+echo "then" >> $name.sh
+echo "echo \"\$res\" | base64 -d | tar -xz -C \$output_dir " >> $name.sh
+echo "else" >> $name.sh
+echo "echo \"\$res\" | base64 -d | tar -xz -C ${name} " >> $name.sh
+echo "fi" >> $name.sh
 
 chmod 777 $name.sh
